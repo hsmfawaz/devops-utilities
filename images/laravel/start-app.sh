3c2348d98a3d5d7a0b210d540851f83cfe67da52
hsmfawaz2 <hsm.fawaz@gmail.com>
2024-03-29 23:08:51 +0200

    add laravel first installation checker

--- images/laravel/start-app.sh
+++ images/laravel/start-app.sh
@@ -2,8 +2,7 @@
 
 chown -R application:application /app
 
-if [ "${APP_QUEUE}" = "supervisor" ]; then
-
+if [ "${APP_QUEUE}" == "supervisor" ]; then
   if [[ ! -e "/opt/docker/etc/supervisor.d/app-queue-worker.conf" ]]; then
     echo ""
     echo "[LOG] Starting App Queue..."
@@ -23,35 +22,35 @@ else
   fi
 fi
 
-if [ "${APP_QUEUE}" = "horizon" ]; then
-  echo ""
-  echo "[LOG] Starting Horizon..."
-  echo ""
+if [ "${APP_QUEUE}" == "horizon" ]; then
   if [[ ! -e "/opt/docker/etc/supervisor.d/app-horizon.conf" ]]; then
+    echo ""
+    echo "[LOG] Starting Horizon..."
+    echo ""
     mv /opt/docker/etc/supervisor.d/app-horizon.conf.x /opt/docker/etc/supervisor.d/app-horizon.conf
   fi
   supervisorctl start app-horizon
 else
-  echo ""
-  echo "[LOG] Stopping Horizon..."
-  echo ""
   if [[ -e "/opt/docker/etc/supervisor.d/app-horizon.conf" ]]; then
+    echo ""
+    echo "[LOG] Stopping Horizon..."
+    echo ""
     supervisorctl stop app-horizon
     mv /opt/docker/etc/supervisor.d/app-horizon.conf /opt/docker/etc/supervisor.d/app-horizon.conf.x
   fi
 fi
 
-if [ "${APP_SCHEDULE}" = "true" ]; then
+if [ "${APP_SCHEDULE}" == "true" ]; then
   echo ""
   echo "[LOG] Starting Cronjob..."
   echo ""
-  echo -n "" >  etc/cron.d/webdevops-docker
+  echo -n "" >  /etc/cron.d/webdevops-docker
   docker-cronjob '* * * * * application /usr/local/bin/php /app/artisan schedule:run'
 else
   echo ""
   echo "[LOG] Clearing Cronjob..."
   echo ""
-  echo -n "" >  etc/cron.d/webdevops-docker
+  echo -n "" >  /etc/cron.d/webdevops-docker
 fi
 
 #check if vendor folder exists and composer.json exists
